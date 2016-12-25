feded776b46dfa54760b66cd59477efa28057cfb
Changwon Choe <qwefgh90@naver.com>
2016-12-25 21:35:30 +0900

    Update init.el

--- init.el
+++ init.el
@@ -1,54 +1,4 @@
-(prefer-coding-system 'utf-8)
 
-;; https://www.emacswiki.org/emacs/RecentFiles
-;; open recent files
-(require 'recentf)
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
-
-;; reference
-;; http://ensime.github.io/editors/emacs/learning/
-;; global variables
-(setq
- inhibit-startup-screen t
- create-lockfiles nil
- make-backup-files nil
- column-number-mode t
- scroll-error-top-bottom t
- show-paren-delay 0.5
- use-package-always-ensure t
- sentence-end-double-space nil)
-
-;; buffer local variables
-(setq-default
- indent-tabs-mode nil
- tab-width 4
- c-basic-offset 4)
-
-;; modes
-;; (electric-indent-mode 0)
-
-;; global keybindings
-(global-unset-key (kbd "C-z"))
-
-;; the package manager
-(require 'package)
-(setq
- package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                    ("org" . "http://orgmode.org/elpa/")
-                    ("melpa" . "http://melpa.org/packages/")
-                    ("melpa-stable" . "http://stable.melpa.org/packages/")))
-
-(package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-
-(use-package ensime
-  :ensure t
-  :pin melpa-stable)
 (prefer-coding-system 'utf-8)
 
 (require 'recentf)
@@ -124,4 +74,3 @@
 
 ;; for update packages in MELPA
 ;; M-x list-packages [RETURN] U [RETURN] x [RETURN]
-
