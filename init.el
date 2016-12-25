d4a5174580d7f64287feed6629d0e4ab0d4e8dc1
Changwon Choe <qwefgh90@naver.com>
2016-12-25 20:40:28 +0900

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
+(prefer-coding-system 'utf-8)
+
+;;recent find open
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
 ;; global variables
 (setq
  inhibit-startup-screen t
@@ -16,7 +24,54 @@
  c-basic-offset 4)
 
 ;; modes
-(electric-indent-mode 0)
+;; (electric-indent-mode 0)
+
+;; global keybindings
+(global-unset-key (kbd "C-z"))
+
+;; the package manager
+(require 'package)
+(setq
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                    ("org" . "http://orgmode.org/elpa/")
+                    ("melpa" . "http://melpa.org/packages/")
+                    ("melpa-stable" . "http://stable.melpa.org/packages/")))
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package ensime
+  :ensure t
+  :pin melpa-stable)
+(prefer-coding-system 'utf-8)
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+;; global variables
+(setq
+ inhibit-startup-screen t
+ create-lockfiles nil
+ make-backup-files nil
+ column-number-mode t
+ scroll-error-top-bottom t
+ show-paren-delay 0.5
+ use-package-always-ensure t
+ sentence-end-double-space nil)
+
+;; buffer local variables
+(setq-default
+ indent-tabs-mode nil
+ tab-width 4
+ c-basic-offset 4)
+
+;; modes
+;; (electric-indent-mode 0)
 
 ;; global keybindings
 (global-unset-key (kbd "C-z"))
